b9d2d87d7a1889726b1770c99dffcd1133c0a3e9
OmChillure <omchillure4@gmail.com>
2024-06-27 05:42:15 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,20 @@
 from nada_dsl import *
 
-
 def nada_main():
+    #Define the party
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    #Define the Secret inputs
+    my_int1 = SecretInteger(Input(name="my_int1", party=party1))  # Current salary
+    my_int2 = SecretInteger(Input(name="my_int2", party=party1))  # Proposed new salary
 
-    return [Output(my_int1, "my_output", party1)]
+    # Calculate the difference
+    salary_difference = my_int1 - my_int2
+
+    # Check if there's an increase
+    is_increase = my_int2 > my_int1
+
+    return [
+        Output(salary_difference, "salary_difference", party1),
+        Output(is_increase, "is_increase", party1)
+    ]
